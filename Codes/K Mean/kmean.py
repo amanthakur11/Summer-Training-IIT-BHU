e34fc551d4babd90a83d74b57d7a4dfbb30de9fc
amanthakur11 <amansingh8412@gmail.com>
2018-07-08 22:53:54 +0530

    Add files via upload

--- Codes/K Mean/kmean.py
+++ Codes/K Mean/kmean.py
@@ -1,1 +1,66 @@
+import pandas
 
+import pylab as pl
+
+from sklearn.cluster import KMeans
+
+from sklearn.decomposition import PCA
+
+
+variables = pandas.read_csv('sample_stocks.csv')
+
+Y = variables[['returns']]
+
+X = variables[['dividendyield']]
+
+
+Nc = range(1, 20)
+
+kmeans = [KMeans(n_clusters=i) for i in Nc]
+
+kmeans
+
+score = [kmeans[i].fit(Y).score(Y) for i in range(len(kmeans))]
+
+score
+
+pl.plot(Nc,score)
+
+pl.xlabel('Number of Clusters')
+
+pl.ylabel('Score')
+
+pl.title('Elbow Curve')
+
+pl.show()
+
+
+
+
+
+pca = PCA(n_components=1).fit(Y)
+
+pca_d = pca.transform(Y)
+
+pca_c = pca.transform(X)
+
+
+
+
+kmeans=KMeans(n_clusters=3)
+
+kmeansoutput=kmeans.fit(Y)
+
+kmeansoutput
+
+pl.figure('3 Cluster K-Means')
+
+pl.scatter(pca_c[:, 0], pca_d[:, 0], c=kmeansoutput.labels_)
+
+pl.xlabel('Dividend Yield')
+
+pl.ylabel('Returns')
+
+pl.title('3 Cluster K-Means')
+
+pl.show()
